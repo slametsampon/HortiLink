e7d2e3c0545b7e7f2e11356db7c3c642035c3ecf
slametsampon <slamet@pon.co.id>
2024-10-27 07:04:24 +0700

    update tahap1/data

--- tahap1/data/script.js
+++ tahap1/data/script.js
@@ -1,10 +1,22 @@
-function sendMessage() {
-  fetch('/sendMessage')
-    .then((response) => response.text())
+// Mengambil data sensor secara berkala dari ESP
+setInterval(() => {
+  fetch('/getData')
+    .then((response) => response.json())
     .then((data) => {
-      document.getElementById('message').innerText = data;
-    })
-    .catch((error) => {
-      console.error('Error:', error);
+      document.getElementById('temperature').innerText = data.temperature;
+      document.getElementById('humidity').innerText = data.humidity;
+      document.getElementById('ph').innerText = data.ph;
+      document.getElementById('ec').innerText = data.ec;
     });
+}, 5000); // Update setiap 5 detik
+
+// Mengirim perintah untuk mengontrol pompa/valve
+function sendCommand(command) {
+  fetch('/control?device=' + command).then((response) => {
+    if (response.ok) {
+      alert('Command sent: ' + command);
+    } else {
+      alert('Failed to send command');
+    }
+  });
 }
